43cd793fc9824392994728bf7e458055142f7475
egathang <elyg3672@gmail.com>
2024-03-10 19:51:01 +0300

    The program to solve a sudoku

---
+++ sudoku.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+	board := parseInput(args)
+
+	// printBoard(board)
+
+	if backtrack(board) {
+		fmt.Println("The Sudoku was solved successfully:")
+		printBoard(board)
+	} else {
+		fmt.Printf("The Sudoku can't be solved.")
+	}
+}
+
+func backtrack(board [][]int) bool {
+	if !hasEmptyCell(board) {
+		return true
+	}
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			if board[i][j] == 0 {
+				for candidate := 9; candidate >= 1; candidate-- {
+					board[i][j] = candidate
+					if isBoardValid(board) {
+						if backtrack(board) {
+							return true
+						}
+						board[i][j] = 0
+					} else {
+						board[i][j] = 0
+					}
+				}
+				return false
+			}
+		}
+	}
+	return false
+}
+
+func hasEmptyCell(board [][]int) bool {
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			if board[i][j] == 0 {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func isBoardValid(board [][]int) bool {
+
+	//check duplicates by row
+	for row := 0; row < 9; row++ {
+		counter := [10]int{}
+		for col := 0; col < 9; col++ {
+			counter[board[row][col]]++
+		}
+		if hasDuplicates(counter) {
+			return false
+		}
+	}
+
+	//check duplicates by column
+	for row := 0; row < 9; row++ {
+		counter := [10]int{}
+		for col := 0; col < 9; col++ {
+			counter[board[col][row]]++
+		}
+		if hasDuplicates(counter) {
+			return false
+		}
+	}
+
+	//check 3x3 section
+	for i := 0; i < 9; i += 3 {
+		for j := 0; j < 9; j += 3 {
+			counter := [10]int{}
+			for row := i; row < i+3; row++ {
+				for col := j; col < j+3; col++ {
+					counter[board[row][col]]++
+				}
+				if hasDuplicates(counter) {
+					return false
+				}
+			}
+		}
+	}
+
+	return true
+}
+
+func hasDuplicates(counter [10]int) bool {
+	for i, count := range counter {
+		if i == 0 {
+			continue
+		}
+		if count > 1 {
+			return true
+		}
+	}
+	return false
+}
+
+func printBoard(board [][]int) {
+	fmt.Println("+-------+-------+-------+")
+	for row := 0; row < 9; row++ {
+		fmt.Print("| ")
+		for col := 0; col < 9; col++ {
+			if col == 3 || col == 6 {
+				fmt.Print("| ")
+			}
+			fmt.Printf("%d ", board[row][col])
+			if col == 8 {
+				fmt.Print("|")
+			}
+		}
+		if row == 2 || row == 5 || row == 8 {
+			fmt.Println("\n+-------+-------+-------+")
+		} else {
+			fmt.Println()
+		}
+	}
+}
+
+func parseInput(args []string) [][]int {
+	board := make([][]int, 9)
+	for i, rowstr := range args {
+		row := make([]int, 9)
+		for j, char := range rowstr {
+			if char == '.' {
+				row[j] = 0
+			} else {
+				row[j] = int(char - '0')
+			}
+		}
+		board[i] = row
+	}
+	return board
+}
